c2cb5c2860f37fe2e0dea4ef96d5b3100a3796af
NaayilS <162499795+NaayilS@users.noreply.github.com>
2024-08-29 00:45:23 -0500

    FINAL ExpenseList.js
    Updated to fit currency changes

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,26 +3,30 @@ import ExpenseItem from './ExpenseItem';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseList = () => {
-    const { expenses } = useContext(AppContext);
+    const { expenses, currency } = useContext(AppContext);
 
     return (
         <table className='table'>
-              <thead className="thead-light">
-            <tr>
-              <th scope="col">Department</th>
-              <th scope="col">Allocated Budget</th>
-              <th scope="col">Increase by 10</th>
-              <th scope="col">Delete</th>
-            </tr>
-          </thead>
+            <thead className="thead-light">
+                <tr>
+                    <th scope="col">Department</th>
+                    <th scope="col">Allocated Budget</th>
+                    <th scope="col">Increase by 10</th>
+                    <th scope="col">Delete</th>
+                </tr>
+            </thead>
             <tbody>
-            {expenses.map((expense) => (
-                <ExpenseItem id={expense.id} key={expense.id} name={expense.name} cost={expense.cost} />
-            ))}
+                {expenses.map((expense) => (
+                    <ExpenseItem 
+                        key={expense.id} 
+                        name={expense.name} 
+                        cost={expense.cost} 
+                        currency={currency} // Pass currency to ExpenseItem
+                    />
+                ))}
             </tbody>
         </table>
     );
 };
 
 export default ExpenseList;
-
